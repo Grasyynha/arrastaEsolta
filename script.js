77169c0b7dad50a117eb9176ed6a678872fc509e
Grasiela Texeira <grasy.mst@gmail.com>
2023-06-28 15:38:47 -0300

    Arrastar e Soltar

--- script.js
+++ script.js
@@ -1,17 +1,72 @@
-document.querySelectorAll('.neutralArea').addEventListener('click', (e) => {
+document.querySelectorAll('.item').forEach(item => {
    item.addEventListener('dragstart', dragStart);
    item.addEventListener('dragend', dragEnd);
 });
 
+document.querySelectorAll('.area').forEach(area => {
+    area.addEventListener('dragover', dragOver);
+    area.addEventListener('dragleave', dragLeave);
+    area.addEventListener('drop', drop);
+
+document.querySelector('.neutralArea').addEventListener('dragover', dragOverNeutral);    
+document.querySelector('.neutralArea').addEventListener('dragleave', dragLeaveNeutral); 
+document.querySelector('.neutralArea').addEventListener('drop', dropNeutral); 
+    
+});
+
 // FUnctions Item
-function dragStart() {
+function dragStart(e) {
     e.currentTarget.classList.add('dragging');
 
 }
 
-function dragEnd() {
+function dragEnd(e) {
     e.currentTarget.classList.remove('dragging');
 
 }
 
 // Functions Area
+
+function dragOver(e){
+    if(e.currentTarget.querySelector('.item') === null ){
+        e.preventDefault();
+        e.currentTarget.classList.add('hover');
+    }
+
+}
+
+function dragLeave(e){
+    e.currentTarget.classList.remove('hover');
+
+}
+
+function drop(e) {
+    e.currentTarget.classList.remove('hover');
+
+    if(e.currentTarget.querySelector('.item') === null ){
+        let dragItem = document.querySelector('.item.dragging');
+        e.currentTarget.appendChild(dragItem);
+    }
+
+
+}
+
+// Function Neutral Areas
+function dragOverNeutral(e){
+    e.preventDefault();
+    e.currentTarget.classList.add('hover');
+
+
+}
+
+function dragLeaveNeutral(e) {
+    e.currentTarget.classList.remove('hover');
+
+}
+
+function dropNeutral(e){
+    e.currentTarget.classList.remove('hover');
+    let dragItem = document.querySelector('.item.dragging');
+    e.currentTarget.appendChild(dragItem);
+
+}
