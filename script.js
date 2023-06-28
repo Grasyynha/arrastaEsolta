49f3375c4bd8dddddf600e213993ca8882ef4c8b
Grasiela Texeira <grasy.mst@gmail.com>
2023-06-28 11:42:49 -0300

    Passo 1

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.querySelector('.neutralArea').addEventListener('click', (e) => {
-    console.log("CLICOU");
+document.querySelectorAll('.neutralArea').addEventListener('click', (e) => {
+   item.addEventListener('dragstart', dragStart);
+   item.addEventListener('dragend', dragEnd);
 });
+
+// FUnctions Item
+function dragStart() {
+    e.currentTarget.classList.add('dragging');
+
+}
+
+function dragEnd() {
+    e.currentTarget.classList.remove('dragging');
+
+}
+
+// Functions Area
